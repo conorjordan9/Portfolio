3838645ea06d9067a28921be645495da70df239f
Waxhoya <waxhoya@gmail.com>
2016-09-20 22:58:44 -0700

    JQuery manipulation implemented.

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,10 +1,10 @@
 var soccerBlog = [
 	{
 		title: "Womens soccer brings playoffs to Portland",
-		summery: "Portland Thorns FC based in Portland Oregon has led the US national women's soccer league in 2016. Because of this the championship game is to be played in Providence park. Providence park was recently in the news for hosting its 100th sellout Timbers game, A stat many fan clubs in the US are Jealous of."
+		summery: "<p>Portland Thorns FC based in Portland Oregon has led the US national women's soccer league in 2016. Because of this the championship game is to be played in Providence park. Providence park was recently in the news for hosting its 100th sellout Timbers game, A stat many fan clubs in the US are Jealous of.</p>"
 	}
 	{
 	title: "Jack Jewsbury to retire at the end of 2016",
-	summery: "The first captain for the Timbers first MLS season is retiring from soccer at the end of this season. When asked what his plans are after soccer he replied with a shrug."
+	summery: "<p>The first captain for the Timbers first MLS season is retiring from soccer at the end of this season. When asked what his plans are after soccer he replied with a shrug.</p>"
 	}
 ]
--- scripts/info.js
+++ scripts/info.js
@@ -1,15 +1,22 @@
 var information = [];
 
-
 function Info (catagories) {
 	this.infoTitle = catagories.info; //Title of info
-	this.summ = catagories.summ; //Summery of info
+	this.summery = catagories.summery; //Summery of info
 }
 
-info.prototype.toHtml = function() {
-	var $infoChunk = $(*******).clone();
+Info.prototype.toHtml = function() {
+	var $infoChunk = $('section').clone();
 	$infoChunk.find("title").html(this.infoTitle);
-	$infoChunk.find("title").html(this.summ);
+	$infoChunk.find(".blogSummery").html(this.summery);
 	$infoChunk.removeATTR('class');
 	return $infoChunk;
-}
+};
+
+soccerBlog.forEach(function(element){
+  information.push(new Info(element));
+});
+
+information.forEach(function(soccerPost){ // Get it?   Soccer... Post...
+  $("#blog").append(soccerPost.toHtml());
+});
