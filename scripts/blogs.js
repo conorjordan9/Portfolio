a075d8cda172aae35b2ef4c1b21ba6560c94e3ce
Waxhoya <waxhoya@gmail.com>
2016-09-21 22:13:08 -0700

    CSS updates for font size, color, and positioning

--- scripts/blogs.js
+++ scripts/blogs.js
@@ -1,14 +1,17 @@
 var soccerBlog = [
 	{
 		title: "Womens soccer brings playoffs to Portland",
+		type: "soccer",
 		summery: "<p>Portland Thorns FC based in Portland Oregon has led the US national women's soccer league in 2016. Because of this the championship game is to be played in Providence park. Providence park was recently in the news for hosting its 100th sellout Timbers game, A stat many fan clubs in the US are Jealous of.</p>"
 	},
 	{
 		title: "Jack Jewsbury to retire at the end of 2016",
+		type: "soccer",
 		summery: "<p>The first captain for the Timbers first MLS season is retiring from soccer at the end of this season. When asked what his plans are after soccer he replied with a shrug.</p>"
   },
 	{
 		title: "Elliptic Curve Crypto: BlockChain Primer",
-		summery: "Today I am going to try to explain Elliptic curve cryptography(ECC). ECC keeps messages and data private by transforming with multiplication based on a public key. When the information is needed again the message is divided by a private key to reveal the message. What keeps ECC secure is the math. Factoring is much more complex then multiplication. Math proofs and significant academic research has identified ECC as practically impossible to crack using existing technology."
+		type: "blockchain",
+		summery: "<p>How to explain Elliptic curve cryptography(ECC). ECC keeps messages and data private by transforming with multiplication based on a public key. When the information is needed again the message is divided by a private key to reveal the message. What keeps ECC secure is the math, factoring is much more complex and resource intensive than multiplication. Math proofs and significant academic research has identified ECC as practically impossible to crack using existing technology.</p>"
 	}
-]
+];
