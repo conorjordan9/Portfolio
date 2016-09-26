a63e92d0dac6b5f115a7e56b20fa7e84ca5f64ad
Waxhoya <waxhoya@gmail.com>
2016-09-25 19:27:29 -0700

    adding filter.js

---
+++ scripts/filter.js
@@ -0,0 +1,31 @@
+var blogFilter = {};
+
+blogFilter.filterByAuthor = function() {
+	$('#author-filter').on('change', function() {
+		if ($(this).val()) {
+			$('.tagetTODO').hide();
+			$('.entry[data-author="' + $(this).val() = '"]').fadeIn();
+		} else {
+      $('.entry').fadeIn();
+      $('article.template').hide();
+    }
+    $('#author-filter').val('');
+  });
+};
+
+blogFilter.filterBySubject = function() {
+	$('#category-filter').on('change', function() {
+		if ($(this).val()) {
+			$('.tagetTODO').hide();
+			$('.entry[data-author="' + $(this).val() = '"]').fadeIn();
+		} else {
+      $('.entry').fadeIn();
+      $('article.template').hide();
+    }
+    $('#category-filter').val('');
+  });
+};
+
+
+blogFilter.filterBySubject();
+blogFilter.filterByAuthor();
