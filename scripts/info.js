6fb40f203abbce10672d1d6c66bc43aedcfa4b4e
Waxhoya <waxhoya@gmail.com>
2016-09-29 14:51:53 -0700

    Integrating Billyham's comments into info.js

--- scripts/info.js
+++ scripts/info.js
@@ -15,12 +15,12 @@ BlogPost.prototype.toHtml = function() {
 };
 
 developerBlog.forEach(function(element){
-  information.push(new Info(element));
+  information.push(new BlogPost(element));
 });
 
 information.forEach(function(bPost){
-  $('#blog').append(bPost.toHtml());
-  console.log('appending story: '+bPost.this.infoTitle);
+  $('#main').append(bPost.toHtml());
+  console.log('appending story: '+bPost.infoTitle);
   $('#author-filter').append('<option value ="' + a.author+ '">' +a.author+ '</option>');
   if ($('#category-filter option[value="' + a.category + '"]').length === 0) {
     $('#category-filter').append('<option value ="' + a.category+ '">' +a.category+ '</option>');
